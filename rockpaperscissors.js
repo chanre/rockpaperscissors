0a8e7160a8ece5a724f34c34e12d58d50800421e
Ryan <ryan.e.chan@gmail.com>
2022-11-07 02:21:43 -0500

    Added win determination and method to restart game
    Game now tracks score and will display who the round, at the very end
    show who the winner is. Implemented a modal/pop-up at the end of the
    game to allow players to easily restart the game.

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -16,54 +16,95 @@ let getComputerChoice = () => {
 function playRound(playerSelection, computerSelection) {
     let player = playerSelection.toLowerCase();
     let winner = "";
+    let score = 0;
+    const winMessage = document.querySelector(".winMessage");
     if (player === "rock" && computerSelection === "scissors" 
     || player === "scissors" && computerSelection === "paper" 
-    || player === "paper" && computerSelection === "rock") {
-        console.log(`You win! ${player} beats ${computerSelection}`); 
+    || player === "paper" && computerSelection === "rock") { 
+        winMessage.textContent = `You win! ${player} beats ${computerSelection}`;
+        score = parseInt(document.querySelector("#playerScore").textContent);
+        score++;
         winner = "player";
+        keepScores(winner, score);
     } else if (player === computerSelection) {
-        console.log("Tie! You both chose the same option");
+        winMessage.textContent = "Tie! You both chose the same option";
         winner = "tie";
     } else {
-        console.log(`You lose! ${computerSelection} beats ${player}`);
+        winMessage.textContent = `You lose! ${computerSelection} beats ${player}`;
+        score = parseInt(document.querySelector("#comScore").textContent);
+        score++;
         winner = "com";
+        keepScores(winner, score);
     }
-    return winner;
 }
 
-function displayScores(winner, wins) {
-    const score = document.querySelector(`#${winner}Score`)
+function keepScores(winner, wins) {
+    const score = document.querySelector(`#${winner}Score`);
     score.textContent = wins;
 }
 
-let playerScore = comScore = 0;
+function openRestart(e) {
+    const modal = document.querySelector(".modal");
+    modal.style.display = "flex";
+}
+
+function closeRestart(e) {
+    const modal = document.querySelector(".modal");
+    const restart = document.querySelector('.restart');
+    if (e.target == modal || e.target == restart) modal.style.display = "none";
+}
+
+function gameOver() {
+    player = parseInt(document.querySelector("#playerScore").textContent);
+    com = parseInt(document.querySelector("#comScore").textContent);
+    if (player === 5) {
+        return "player";
+    } else if (com === 5) {
+        return "com";
+    } else {
+        return false;
+    }
+}
+
+function displayWinner(winner) {
+    const winMessage = document.querySelector('.winMessage');
+    if (winner === "com") winner = winner.toUpperCase();
+    winMessage.textContent = `The ${winner} is the winner!`; 
+}
+
+function reset() {
+    player = document.querySelector("#playerScore");
+    com = document.querySelector("#comScore");
+    winMessage = document.querySelector(".winMessage");
+    player.textContent = 0;
+    com.textContent = 0;
+    winMessage.textContent = "Good luck!";
+}
+
+function game(e) {
+    let winner = gameOver();
+    if (winner != false) {
+        openRestart();
+        return;
+    } 
+
+    playRound(e.target.id, getComputerChoice());
+    winner = gameOver();
+
+    if (winner != false) {
+        displayWinner(winner);
+        openRestart();
+        return;
+    } 
+
+}
 
 const buttons = document.querySelectorAll('.buttons');
+const restart = document.querySelector('.restart');
+
+restart.addEventListener('click', reset);
 buttons.forEach(button => {
-    button.addEventListener('click', () => {
-        playRound(button.id, getComputerChoice());
-    });
+    button.addEventListener('click', game);
 });
 
-
-
-// function game() {
-//     let player = 0;
-//     let computer = 0;
-//     for (let i = 0; i < 5; i++) {
-//         let round = playRound(prompt("Rock, paper or scissors?", "Rock"), getComputerChoice());
-//         if (round === "player") {
-//             player++;
-//         } else if (round === "computer") {
-//             computer++;
-//         }
-//     }   
-
-//     if (player > computer) {
-//         console.log("You won!");
-//     } else if (player === computer) {
-//         console.log("You tied!");
-//     } else {
-//         console.log("Computer won!");
-//     }  
-// }
+window.document.addEventListener('click', closeRestart);
