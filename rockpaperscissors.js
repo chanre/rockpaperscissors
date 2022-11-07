4b90e6cb02eb5a8ff857f2ecf5d7ac5a4a561862
Ryan <ryan.e.chan@gmail.com>
2022-11-06 23:06:32 -0500

    Add button interactivity and score template
    Buttons can be pressed now and console has basic functionality with
    the ability to play individual rounds.

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -26,28 +26,44 @@ function playRound(playerSelection, computerSelection) {
         winner = "tie";
     } else {
         console.log(`You lose! ${computerSelection} beats ${player}`);
-        winner = "computer";
+        winner = "com";
     }
     return winner;
 }
 
-function game() {
-    let player = 0;
-    let computer = 0;
-    for (let i = 0; i < 5; i++) {
-        let round = playRound(prompt("Rock, paper or scissors?", "Rock"), getComputerChoice());
-        if (round === "player") {
-            player++;
-        } else if (round === "computer") {
-            computer++;
-        }
-    }   
-
-    if (player > computer) {
-        console.log("You won!");
-    } else if (player === computer) {
-        console.log("You tied!");
-    } else {
-        console.log("Computer won!");
-    }  
+function displayScores(winner, wins) {
+    const score = document.querySelector(`#${winner}Score`)
+    score.textContent = wins;
 }
+
+let playerScore = comScore = 0;
+
+const buttons = document.querySelectorAll('.buttons');
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        playRound(button.id, getComputerChoice());
+    });
+});
+
+
+
+// function game() {
+//     let player = 0;
+//     let computer = 0;
+//     for (let i = 0; i < 5; i++) {
+//         let round = playRound(prompt("Rock, paper or scissors?", "Rock"), getComputerChoice());
+//         if (round === "player") {
+//             player++;
+//         } else if (round === "computer") {
+//             computer++;
+//         }
+//     }   
+
+//     if (player > computer) {
+//         console.log("You won!");
+//     } else if (player === computer) {
+//         console.log("You tied!");
+//     } else {
+//         console.log("Computer won!");
+//     }  
+// }
